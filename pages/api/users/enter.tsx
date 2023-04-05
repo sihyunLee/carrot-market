db3d901592fb4c02348d6917405c7bd819c9c0f7
sihyun <sogo91q@naver.com>
2023-04-05 17:05:44 +0900

    REFACTORING

---
+++ pages/api/users/enter.tsx
@@ -0,0 +1,10 @@
+import withHandler from "@/libs/server/withHandler";
+import { NextApiRequest, NextApiResponse } from "next";
+import client from "../../../libs/server/client";
+
+async function handler(req: NextApiRequest, res: NextApiResponse) {
+  console.log(req.body);
+  return res.status(200).end();
+}
+
+export default withHandler("POST", handler);
